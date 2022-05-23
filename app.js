ab5b29c021a034eef62755484f022558ca0b066f
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2022-05-23 19:00:44 +0530

    post route

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function (req, res) {
+app.post("/calculator", function (req, res) {
   let num1 = Number(req.body.num1);
   let num2 = Number(req.body["num2"]);
   let operator = "";
